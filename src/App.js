b5d928a2b58b93376ffe79f3e6e11c73a71d64b8
sgbo5003 <sgbo5003@github.com>
2024-04-23 17:19:52 +0900

    ➕ react-slick 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="relative w-full mx-auto my-0">
       <Header />
-      <div>hi</div>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,26 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
 
 const Header = () => {
   return (
     <div id="header" className="relative w-full">
-      <div className="headerTop">
+      <div className="headerTop border-b border-[#eaeaea]">
         <div className="after:clear-both after:block max-w-[1240px] mx-auto my-0">
           <div className="float-left">
-            <div className="py-25">
+            <div className="py-9">
               <a href="#!">
                 <img
-                  className="w-12 h-12"
+                  className="w-8 h-8"
                   src="image/shiba.png"
                   alt="홈 이미지"
                 />
@@ -17,14 +28,77 @@ const Header = () => {
             </div>
           </div>
           <div className="float-right">
-            <ul className="py-25">
-              <li className="float-left ml-20">로그인</li>
-              <li className="float-left ml-20">회원가입</li>
-              <li className="float-left ml-20">마이페이지</li>
-              <li className="float-left ml-20">장바구니</li>
+            <div>
+              <ul>
+                <li className="float-left ml-20">
+                  <a className="leading-[50px]" href="#!">
+                    로그인
+                  </a>
+                </li>
+                <li className="float-left ml-20">
+                  <a className="leading-[50px]" href="#!">
+                    회원가입
+                  </a>
+                </li>
+                <li className="float-left ml-20">
+                  <a className="leading-[50px]" href="#!">
+                    마이페이지
+                  </a>
+                </li>
+                <li className="float-left ml-20">
+                  <a className="leading-[50px]" href="#!">
+                    장바구니
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="headerContent border-b border-[#eaeaea]">
+        <div className="max-w-[1240px] mx-auto my-0 bg-orange-200 h-36"></div>
+      </div>
+      <div className="headerGnb border-b border-[#eaeaea] h-12">
+        <div className="max-w-[1240px] mx-auto my-0 h-full">
+          <div className="h-full">
+            <ul className="flex items-center justify-around h-full">
+              <li>
+                <a href="#!">강아지 상품</a>
+              </li>
+              <li>
+                <a href="#!">게시판</a>
+              </li>
+              <li>
+                <a href="#!">리뷰</a>
+              </li>
+              <li>
+                <a href="#!">방문 신청</a>
+              </li>
             </ul>
           </div>
         </div>
+      </div>
+      <div className="h-96">
+        <Slider {...settings}>
+          <div>
+            <h3 className="bg-green-700 text-center leading-[670px]">1</h3>
+          </div>
+          <div>
+            <h3 className="bg-green-700 text-center leading-[670px]">2</h3>
+          </div>
+          <div>
+            <h3 className="bg-green-700 text-center leading-[670px]">3</h3>
+          </div>
+          <div>
+            <h3 className="bg-green-700 text-center leading-[670px]">4</h3>
+          </div>
+          <div>
+            <h3 className="bg-green-700 text-center leading-[670px]">5</h3>
+          </div>
+          <div>
+            <h3 className="bg-green-700 text-center leading-[670px]">6</h3>
+          </div>
+        </Slider>
       </div>
     </div>
   );
