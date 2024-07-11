4c9b8ee5a8bfd60030d78d5c70b53d33ca8714c9
sgbo5003 <sgbo5003@naver.com>
2024-07-12 02:43:39 +0900

    7/11 바로 결제 UI 추가

--- src/components/product/ProductCheckOut.tsx
+++ src/components/product/ProductCheckOut.tsx
@@ -3,26 +3,94 @@ import React from "react";
 const ProductCheckOut = () => {
   return (
     <div className="relative w-full mx-auto my-0">
-      <div className="max-w-[1240px] mx-auto my-0">
+      <div className="max-w-[1240px] mx-auto my-[2.5rem]">
         <div>
           <form className="xl:gap-x-16 lg:gap-x-12 lg:grid-cols-2 lg:grid">
             <div>
               <div>
                 <h2 className="text-lg font-medium text-gray-900">
-                  Contact information
+                  주문자 정보
                 </h2>
                 <div className="mt-[1rem]">
                   <label
-                    htmlFor="emailAdress"
+                    htmlFor="orderer"
                     className="block text-sm font-medium text-gray-700"
                   >
-                    Email address
+                    주문하시는 분
                   </label>
                   <div className="mt-[0.25rem]">
                     <input
                       type="email"
-                      name="emailAdress"
-                      id="emailAdress"
+                      name="orderer"
+                      id="orderer"
+                      autoComplete="email"
+                      className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
+                    />
+                  </div>
+                </div>
+                <div className="mt-[1rem]">
+                  <label
+                    htmlFor="address"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    주소
+                  </label>
+                  <div className="mt-[0.25rem]">
+                    <input
+                      type="email"
+                      name="address"
+                      id="address"
+                      autoComplete="email"
+                      className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
+                    />
+                  </div>
+                </div>
+                <div className="mt-[1rem]">
+                  <label
+                    htmlFor="phoneNumber1"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    전화번호
+                  </label>
+                  <div className="mt-[0.25rem]">
+                    <input
+                      type="email"
+                      name="phoneNumber1"
+                      id="phoneNumber1"
+                      autoComplete="email"
+                      className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
+                    />
+                  </div>
+                </div>
+                <div className="mt-[1rem]">
+                  <label
+                    htmlFor="phoneNumber2"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    휴대폰 번호
+                  </label>
+                  <div className="mt-[0.25rem]">
+                    <input
+                      type="email"
+                      name="phoneNumber2"
+                      id="phoneNumber2"
+                      autoComplete="email"
+                      className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
+                    />
+                  </div>
+                </div>
+                <div className="mt-[1rem]">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    이메일
+                  </label>
+                  <div className="mt-[0.25rem]">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
                       autoComplete="email"
                       className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
                     />
@@ -30,54 +98,100 @@ const ProductCheckOut = () => {
                 </div>
               </div>
               <div className="pt-[2.5rem] border-gray-200 border-t mt-[2.5rem]">
-                <h2 className="text-lg font-medium text-gray-900">
-                  Shipping information
-                </h2>
-                <div className="sm:gap-x-4 sm:grid-cols-2 gap-y-6 grid-cols-1 grid mt-[1rem]">
+                <h2 className="text-lg font-medium text-gray-900">배송 정보</h2>
+                <div className="mt-[1rem]">
                   <div>
-                    <label
-                      htmlFor="firstName"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      first Name
-                    </label>
-                    <div className="mt-[0.25rem]">
-                      <input
-                        type="text"
-                        name="firstName"
-                        id="firstName"
-                        className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="lastName"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      last Name
-                    </label>
-                    <div className="mt-[0.25rem]">
-                      <input
-                        type="text"
-                        name="lastName"
-                        id="lastName"
-                        className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
-                      />
-                    </div>
-                  </div>
-                  <div className="sm:col-span-2">
                     <label
                       htmlFor="company"
                       className="block text-sm font-medium text-gray-700"
                     >
-                      Company
+                      배송지 확인
                     </label>
                     <div className="mt-[0.25rem]">
                       <input
                         type="text"
                         name="company"
                         id="company"
+                        className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="mt-[1rem]">
+                    <label
+                      htmlFor="receiver"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      받으실분
+                    </label>
+                    <div className="mt-[0.25rem]">
+                      <input
+                        type="text"
+                        name="receiver"
+                        id="receiver"
+                        className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="mt-[1rem]">
+                    <label
+                      htmlFor="address2"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      받으실 곳
+                    </label>
+                    <div className="mt-[0.25rem]">
+                      <input
+                        type="text"
+                        name="address2"
+                        id="address2"
+                        className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="mt-[1rem]">
+                    <label
+                      htmlFor="phoneNumber3"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      전화번호
+                    </label>
+                    <div className="mt-[0.25rem]">
+                      <input
+                        type="text"
+                        name="phoneNumber3"
+                        id="phoneNumber3"
+                        className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="mt-[1rem]">
+                    <label
+                      htmlFor="phoneNumber4"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      휴대폰 번호
+                    </label>
+                    <div className="mt-[0.25rem]">
+                      <input
+                        type="text"
+                        name="phoneNumber4"
+                        id="phoneNumber4"
+                        className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="mt-[1rem]">
+                    <label
+                      htmlFor="requestMessage"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      요청사항
+                    </label>
+                    <div className="mt-[0.25rem]">
+                      <input
+                        type="text"
+                        name="requestMessage"
+                        id="requestMessage"
                         className="block w-full border-gray-300 rounded-md shadow-sm sm:text-sm"
                       />
                     </div>
