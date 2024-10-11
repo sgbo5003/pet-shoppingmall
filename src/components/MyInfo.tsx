36e747e238852c38f119fd561bb138e83f7e0318
sgbo5003 <sgbo5003@naver.com>
2024-10-11 11:14:12 +0900

    💄 내 정보 수정 화면 UI 추가

--- src/components/MyInfo.tsx
+++ src/components/MyInfo.tsx
@@ -10,6 +10,41 @@ const MyInfo = () => {
                 회원정보 변경
               </h2>
             </div>
+            <div>
+              <form>
+                <div>
+                  <h3 className="pb-17 text-lg text-[#222222] inline-block">
+                    기본정보
+                  </h3>
+                  <div className="border-t border-[#999999]">
+                    <table className="w-full">
+                      <colgroup>
+                        <col width="25%" />
+                        <col width="75%" />
+                      </colgroup>
+                      <tbody>
+                        <tr>
+                          <th className="text-left border-b border-[#dcdcdc] bg-[#fbfbfb] py-10 px-25 border-l-0">
+                            <span>아이디</span>
+                          </th>
+                          <td className="py-15 pl-15 border-b border-[#dcdcdc]">
+                            sgbo5003
+                          </td>
+                        </tr>
+                        <tr>
+                          <th className="text-left border-b border-[#dcdcdc] bg-[#fbfbfb] py-10 px-25 border-l-0">
+                            <span>아이디</span>
+                          </th>
+                          <td className="py-15 pl-15 border-b border-[#dcdcdc]">
+                            sgbo5003
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
